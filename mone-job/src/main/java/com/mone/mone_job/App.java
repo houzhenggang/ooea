c0a7a08f8d48c05f89e0d874dbe30fa3486864eb
bc <201692131@qq.com>
2016-06-21 22:26:36 +0800

    Change-Id: I731c1c7b4024ea95564fb5e12f8190f1a39a18c4

---
+++ mone-job/src/main/java/com/mone/mone_job/App.java
@@ -0,0 +1,13 @@
+package com.mone.mone_job;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
