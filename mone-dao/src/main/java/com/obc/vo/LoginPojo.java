8ffc27c1fd460a402ea86a0d8c881b764a15d533
bc <201692131@qq.com>
2016-06-21 22:10:29 +0800

    Change-Id: I6c718ec898857180b15f60b65a6687719accf5ea

---
+++ mone-dao/src/main/java/com/obc/vo/LoginPojo.java
@@ -0,0 +1,40 @@
+package com.obc.vo;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * <br>类名： LoginPojo 
+ *
+ * <br>公司名称： 【】
+ * <br>描述：【】
+ * <br>创建时间： 2016年5月31日 上午2:19:23 
+ * @author FC
+ */
+public class LoginPojo implements Serializable {
+	
+	/**
+	 * <br>创建时间： 2016年5月31日 上午2:19:09
+	 * @Fields serialVersionUID : TODO 【】
+	 */
+	private static final long serialVersionUID = -5748856127917166189L;
+	private String			  username;
+	private String			  password;
+							  
+	public String getUsername ( ) {
+		return username;
+	}
+	
+	public void setUsername ( String username ) {
+		this.username = username;
+	}
+	
+	public String getPassword ( ) {
+		return password;
+	}
+	
+	public void setPassword ( String password ) {
+		this.password = password;
+	}
+	
+}
---
+++ mone-dao/src/main/java/org/aliyun/Aliyun.java
@@ -0,0 +1,5 @@
+package org.aliyun;
+
+public interface Aliyun {
+
+}
