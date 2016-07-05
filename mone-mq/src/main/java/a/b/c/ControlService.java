1ce1d58a9968805097670f14044438844b03eb1c
方超 <fc@yaozhitech.com>
2016-07-05 16:39:42 +0800

    Change-Id: Ib6068afbcdd2820f82eb6bf6c9108c7331b37c02

--- mone-mq/src/main/java/a/b/c/ControlService.java
+++ mone-mq/src/main/java/a/b/c/ControlService.java
@@ -1,6 +1,24 @@
 package a.b.c;
 
+/**
+ * 
+ * <br>类名： ControlService 
+ *
+ * <br>公司名称： 【】
+ * <br>描述：【】
+ * <br>创建时间： 2016年7月5日 下午4:35:57 
+ * @author dogesoft
+ */
 public interface ControlService<T, E> {
 	
-	T exec ( E request );
+	/**
+	 * 
+	 * <br>方法名： exec
+	 * 
+	 * <br>描述：【】 
+	 * <br>创建时间： 2016年7月5日 下午4:36:05 
+	 * @param param E
+	 * @return T
+	 */
+	T exec ( E param );
 }
