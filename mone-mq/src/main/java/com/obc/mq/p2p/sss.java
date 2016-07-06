20e377fbeb5c722710f95d61462143eed4b6d440
方超 <fc@yaozhitech.com>
2016-07-06 16:52:57 +0800

    Change-Id: I017f4d2c47169221212b2ae5102f01e7430cc72a

--- mone-mq/src/main/java/com/obc/mq/p2p/sss.java
+++ mone-mq/src/main/java/com/obc/mq/p2p/sss.java
@@ -9,16 +9,27 @@ public class sss implements SK {
 	@Override
 	public MessageReq dkem ( MqService s ) {
 		// TODO Auto-generated method stub
+		
 		return new MessageReq();
 	}
 	
 	public static void main ( String[] args ) {
-		MessageReq s =new sss().dkem(new MqService<MessageResp, MessageReq>() {
+		MessageReq s = new sss().dkem(new MqService<MessageResp, MessageReq>() {
 			@Override
 			public MessageResp exec ( MessageReq param ) {
 				// TODO Auto-generated method stub
+				System.out.println("ssdfsa"+param);
 				return new MessageResp();
 			}
+			
+			
 		});
+		
+		
+		KeyBroad kb = new KeyBroad();
+		NetCardBroad ncb = new NetCardBroad();
+		CheckBroad cb = new CheckBroad();
+		cb.getMainMessage(kb);
+		cb.getMainMessage(ncb);
 	}
 }
