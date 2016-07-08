3249de32a04e980c7fd5283ab36e38eaaa7c1330
bc <201692131@qq.com>
2016-07-09 00:59:33 +0800

    Change-Id: I502864a4e488c47efcf4497c0f9488657921f650

--- mone-producer/src/main/java/com/obc/producer/ProducerService.java
+++ mone-producer/src/main/java/com/obc/producer/ProducerService.java
@@ -2,8 +2,34 @@ package com.obc.producer;
 
 import java.util.Map;
 
+/**
+ * 
+ * <br>类名： ProducerService 
+ *
+ * <br>公司名称： 【】
+ * <br>描述：【生产者、根据不同的方法生产不同的消息】
+ * <br>创建时间： 2016年7月9日 上午12:35:27 
+ * @author FC
+ */
 public interface ProducerService {
 	
+	/**
+	 * 
+	 * <br>方法名： smsSendMessage
+	 * 
+	 * <br>描述：【短信】 
+	 * <br>创建时间： 2016年7月9日 上午12:35:15 
+	 * @param obj
+	 */
 	void smsSendMessage ( Map<String, Object> obj );
 	
+	/**
+	 * 
+	 * <br>方法名： emailSendMessage
+	 * 
+	 * <br>描述：【email】 
+	 * <br>创建时间： 2016年7月9日 上午12:35:19 
+	 * @param obj
+	 */
+	void emailSendMessage ( Map<String, Object> obj );
 }
--- mone-producer/src/main/java/com/obc/producer/impl/ProducerServiceImpl.java
+++ mone-producer/src/main/java/com/obc/producer/impl/ProducerServiceImpl.java
@@ -8,23 +8,45 @@ import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.Session;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+import org.springframework.stereotype.Service;
 
 import com.obc.producer.ProducerService;
 
-
+/**
+ * 
+ * <br>
+ * 类名： ProducerServiceImpl
+ *
+ * <br>
+ * 公司名称： 【】
+ * <br>
+ * 描述：【消息发送】
+ * <br>
+ * 创建时间： 2016年7月9日 上午12:40:52
+ * @author FC
+ */
+@Service( "producerServiceImpl" )
 public class ProducerServiceImpl implements ProducerService {
-	
-	private Destination	smsDestination;
+	/* 消息模板 */
 	private JmsTemplate	jmsTemplate;
+	/* 短信消息标记 */
+	private Destination	smsDestination;
+	/* email消息标记 */
+	private Destination	emailDestination;
+						
+	public void setJmsTemplate ( JmsTemplate jmsTemplate ) {
+		this.jmsTemplate = jmsTemplate;
+	}
 	
 	public void setSmsDestination ( Destination smsDestination ) {
 		this.smsDestination = smsDestination;
 	}
 	
-	public void setJmsTemplate ( JmsTemplate jmsTemplate ) {
-		this.jmsTemplate = jmsTemplate;
+	public void setEmailDestination ( Destination emailDestination ) {
+		this.emailDestination = emailDestination;
 	}
 	
 	@Override
@@ -39,6 +61,20 @@ public class ProducerServiceImpl implements ProducerService {
 				}
 				return mm;
 			}
+		});
+		
+	}
+	
+	@Override
+	public void emailSendMessage ( Map<String, Object> obj ) {
+		jmsTemplate.send(emailDestination, new MessageCreator() {
+			
+			@Override
+			public Message createMessage ( Session arg0 ) throws JMSException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
 		});
 		
 	}
