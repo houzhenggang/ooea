2f198100273a780510398219aed1fdfac4d4e017
方超 <fc@yaozhitech.com>
2016-07-08 13:37:54 +0800

    Change-Id: I987d381af405acd586152af8053192ed8a029c70

--- mone-consumer/src/main/java/com/obc/mq/ConsumerService.java
+++ mone-consumer/src/main/java/com/obc/mq/ConsumerService.java
@@ -4,7 +4,7 @@ package com.obc.mq;
  * <br>类名： MqService 
  *
  * <br>公司名称： 【】
- * <br>描述：【mq消息接口】
+ * <br>描述：【mq消息接口(消费者)】
  * <br>创建时间： 2016年7月5日 下午7:35:36 
  * @author FC
  */
@@ -13,7 +13,7 @@ public interface ConsumerService<T, E> {
 	/**
 	 * <br>方法名： exec
 	 * 
-	 * <br>描述：【】 
+	 * <br>描述：【消费者接口】 
 	 * <br>创建时间： 2016年7月5日 下午7:35:32 
 	 * @param param E消息体
 	 * @return T消息响应
