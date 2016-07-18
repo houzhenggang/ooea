823c8562aa70e7301371137139fa2886819c81c3
方超 <fc@yaozhitech.com>
2016-07-18 11:19:11 +0800

    Change-Id: I3f98747b778b6687cabf58fb84a34447c511e1d6

--- mone-commons/src/main/java/com/obc/common/utils/jedis/JedisTransaction.java
+++ mone-commons/src/main/java/com/obc/common/utils/jedis/JedisTransaction.java
@@ -1,5 +1,19 @@
 package com.obc.common.utils.jedis;
 
+import redis.clients.jedis.Jedis;
+
 public class JedisTransaction{
+	StringRedisTemplate
 	
+	public static Object multi(Jedis jedis,String key){
+		return jedis.multi();
+	}
+	
+	public static Object unwatch(Jedis jedis,String key){
+		return jedis.unwatch();
+	}
+	
+	public static Object watch(Jedis jedis,String...keys){
+		return jedis.watch(keys);
+	}
 }
